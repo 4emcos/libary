459b6ab447391a41c673a5cc1c7a0a00184b0c9d
Emerson Costa <emerson.esn@gmail.com>
2020-04-13 17:01:12 -0300

    commit inicial

---
+++ src/components/Books.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+        <h1>Livros</h1>
+        <hr/>
+        <LibaryForm v-on:emit-content="onSubmit" />
+        <b-container class="bv-example-row">
+            <b-row>
+            <div v-for="(i, index) in books" :key="index">
+                    <b-col>
+                        <div>
+                        <b-card-group deck>
+                            <b-card header-tag="header" footer-tag="footer">
+
+                            <template v-slot:header>
+                                <h5 class="mb-0"> <span> <strong>{{ i.titulo }}</strong></span> </h5>
+                            </template>
+                            
+                            <b-card-text>
+                                    <span>Autor: <strong>{{ i.autor }}</strong></span> <br/>
+                                    <span>Ano: <strong>{{ i.ano }}</strong></span> <br/>
+                                <span>Editora: <strong>{{ i.editora }}</strong></span> <br/>
+                                <span>Observações: <strong>{{ i.observacoes }}</strong></span>
+                            
+                            </b-card-text>
+                            
+                            <template v-slot:footer>
+                                <b-button v-on:click.prevent="removeBook(index)" href="#" variant="primary">Excluir</b-button>
+                            </template>
+                            </b-card>
+                        </b-card-group>
+                        </div>
+                    </b-col>
+        
+            </div>
+            </b-row>
+        </b-container>
+
+      </div>
+</template>
+
+<script>
+
+import LibaryForm from './LibaryForm.vue';
+
+export default {
+    name: "Books",
+    components:{
+        LibaryForm
+    },
+    data(){
+        return{
+           books: [] 
+        }
+    },
+    methods: {
+        onSubmit(content){
+            this.books.push(content);
+        
+        },
+        removeBook(index){            
+            this.books.splice(index, 1);
+        }
+    }
+}
+</script>
---
+++ src/components/LibaryForm.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+
+      <b-form-group id="input-group-1" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="form.titulo"
+          required
+          placeholder="Nome do livro"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.ano"
+          required
+          placeholder= "Ano de lançamento"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.autor"
+          required
+          placeholder="Autor"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          v-model="form.editora"
+          required
+          placeholder="Editora"
+        ></b-form-input>
+      </b-form-group>
+
+
+      <b-form-group id="input-group-5" label-for="input-5">
+        <b-form-input
+          id="input-5"
+          v-model="form.observacoes"
+          placeholder="Observações"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+        &nbsp;
+      <b-button type="reset" variant="danger">Reset</b-button>
+
+
+    </b-form>    
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LibaryForm",
+    data() {
+      return {
+        form: {
+          titulo: '',
+          ano: '',
+          autor: '',
+          editora: '',
+          observacoes: '',
+
+        },
+        //foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(content) {
+        content.preventDefault()
+         this.$emit("emit-content", {titulo: this.form.titulo, 
+                                     ano: this.form.ano,
+                                     autor: this.form.autor,
+                                     editora: this.form.editora,
+                                     observacoes: this.form.observacoes,                   
+                                     });
+                                     
+      },
+      onReset(content) {
+        content.preventDefault()
+
+        this.form.titulo = ''
+        this.form.ano = ''
+        this.form.autor = ''
+        this.form.editora = ''
+        this.form.observacoes = ''
+      
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
